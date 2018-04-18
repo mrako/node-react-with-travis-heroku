afc1d437b731d80196571d8e89a74692ed9a2e99
Sonja Somero <sonja.somero@helsinki.fi>
2018-04-18 19:39:47 +0300

    Fix test

--- backend/test/helloworld.test.js
+++ backend/test/helloworld.test.js
@@ -2,9 +2,10 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('hello world works', () => {
-  it('should return a message when requesting hello', async () => {
+  it('should return a message when requesting hello', (done) => {
     request('http://localhost:9000/api/v1/hello/world', (error, response, body) => {
       expect(body).to.equal('Hello World from backend');
+      done();
     });
   });
 });
